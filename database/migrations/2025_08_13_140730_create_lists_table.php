76e4857b74381fd1023979e796b5c8485d82e586
Nawon <nawon2794@gmail.com>
2025-08-17 15:35:29 +0530

    Add description column to lists table migration✏️

--- database/migrations/2025_08_13_140730_create_lists_table.php
+++ database/migrations/2025_08_13_140730_create_lists_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('lists', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
