ae8c99bb4433cd38b37fcbe99792852ce2211a77
Nawon <nawon2794@gmail.com>
2025-08-17 16:37:12 +0530

    Add tasks table migration with title, description, is_completed, due_date, and user_id fields✏️

--- database/migrations/2025_08_13_140808_create_tasks_table.php
+++ database/migrations/2025_08_13_140808_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('is_completed')->default(false);
+            $table->dateTime('due_date')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
             $table->timestamps();
+            
         });
     }
 
